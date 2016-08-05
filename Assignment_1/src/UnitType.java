e3040348bcb25b8005dd34dd03664d1178e592ab
Ed <rumed12@gmail.com>
2016-08-05 16:35:04 +1200

    Created Unit type

---
+++ Assignment_1/src/UnitType.java
@@ -0,0 +1,10 @@
+
+public enum UnitType {
+	DogHandler("Dog Handler"), Officer("Police Officer");
+	
+	private final String unitTitle;
+	
+	private UnitType( String unitTitle ) {
+		this.unitTitle = unitTitle;
+	}
+}
